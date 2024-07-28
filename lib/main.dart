966e3abec25aedc1447637123dfa05e33d767fdc
Admin <maayush235@gmail.com>
2024-07-28 13:43:35 +0530

    Radhey radhey

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import 'day1/view/homescreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,17 +9,10 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
       home: const HomeScreen(),
     );
   }
