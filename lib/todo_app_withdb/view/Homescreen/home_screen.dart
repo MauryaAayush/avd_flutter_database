5040c6b21ce4fe0f9ba2c9e6d4a5dd11a368d092
Admin <maayush235@gmail.com>
2024-07-28 14:07:02 +0530

    Radhey radhey

--- lib/todo_app_withdb/view/Homescreen/home_screen.dart
+++ lib/todo_app_withdb/view/Homescreen/home_screen.dart
@@ -3,6 +3,8 @@ import 'package:avd_flutter_database/todo_app_withdb/model/task_model.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+
+
 class TodoApp extends StatelessWidget {
   final TaskController taskController = Get.put(TaskController());
 
@@ -22,17 +24,42 @@ class TodoApp extends StatelessWidget {
             return Card(
               color: getPriorityColor(task.priority),
               child: ListTile(
-                title: Text(task.taskName),
-                subtitle: Text(task.note ?? ''),
-                trailing: Checkbox(
-                  value: task.isDone,
-                  onChanged: (value) {
-                    taskController.toggleTaskStatus(task);
-                  },
+                title: Text(
+                  task.taskName,
+                  style: TextStyle(
+                    decoration: task.isDone ? TextDecoration.lineThrough : null,
+                  ),
                 ),
-                onLongPress: () {
-                  taskController.deleteTask(task.id!);
-                },
+                subtitle: Text(task.note ?? ''),
+                trailing: Wrap(
+                  spacing: 12, // space between two icons
+                  children: <Widget>[
+                    IconButton(
+                      icon: Icon(Icons.check),
+                      color: task.isDone ? Colors.green : Colors.grey,
+                      onPressed: () {
+                        taskController.toggleTaskStatus(task);
+                      },
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.edit),
+                      onPressed: () {
+                        showDialog(
+                          context: context,
+                          builder: (context) {
+                            return EditTaskDialog(task: task);
+                          },
+                        );
+                      },
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.delete),
+                      onPressed: () {
+                        taskController.deleteTask(task.id!);
+                      },
+                    ),
+                  ],
+                ),
               ),
             );
           },
@@ -114,6 +141,65 @@ class AddTaskDialog extends StatelessWidget {
             Get.back();
           },
           child: Text('Add'),
+        ),
+      ],
+    );
+  }
+}
+
+class EditTaskDialog extends StatelessWidget {
+  final Task task;
+
+  final taskNameController = TextEditingController();
+  final noteController = TextEditingController();
+  final priorityController = TextEditingController();
+
+  EditTaskDialog({required this.task}) {
+    taskNameController.text = task.taskName;
+    noteController.text = task.note ?? '';
+    priorityController.text = task.priority.toString();
+  }
+
+  final TaskController taskController = Get.find();
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text('Edit Task'),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          TextField(
+            controller: taskNameController,
+            decoration: InputDecoration(labelText: 'Task Name'),
+          ),
+          TextField(
+            controller: noteController,
+            decoration: InputDecoration(labelText: 'Note'),
+          ),
+          TextField(
+            controller: priorityController,
+            decoration: InputDecoration(labelText: 'Priority (1, 2, 3)'),
+            keyboardType: TextInputType.number,
+          ),
+        ],
+      ),
+      actions: [
+        TextButton(
+          onPressed: () {
+            Get.back();
+          },
+          child: Text('Cancel'),
+        ),
+        TextButton(
+          onPressed: () {
+            task.taskName = taskNameController.text;
+            task.note = noteController.text;
+            task.priority = int.parse(priorityController.text);
+            taskController.updateTask(task);
+            Get.back();
+          },
+          child: Text('Update'),
         ),
       ],
     );
